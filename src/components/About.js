2aee700956682179e369a8ab005afec78efd907a
Milan Cifra <milan3362@gmail.com>
2018-11-10 11:01:05 +0100

    basic header

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class About extends React.Component {
+    render() {
+        return (
+            <div className="about">
+                <h2>About us</h2>
+            </div>
+        )
+    }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,56 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import {Container} from 'reactstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {Container, 
+        Navbar, 
+        NavbarBrand, 
+        NavbarToggler, 
+        Collapse,
+        Nav, 
+        NavItem, 
+        NavLink} from 'reactstrap';
 import firebase from 'firebase';
 
 import Administration from './Administration';
 import Homepage from './Homepage';
+import About from './About';
+import Contact from './Contact';
 
 class App extends React.Component {
   
   constructor(props) {
     super(props);
+    this.state = {openNav: false};
     // Initialize Firebase
     const firebase_config = require('../firebase_config');
     firebase.initializeApp(firebase_config);
+  }
+
+  toggleNav() {
+    this.setState({openNav: !this.state.openNav});
   }
 
   render() {
     return (
       <Router>
         <Container>
-          <h1>Car Rental</h1>
-          <ul>
-            <li><Link to={'/'}>Homepage</Link></li>
-            <li><Link to={'/administration'}>Administration</Link></li>
-          </ul>
-          <hr />
+          <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">Car Rental</NavbarBrand>
+            <NavbarToggler onClick={() => this.toggleNav()} />
+            <Collapse isOpen={this.state.openNav} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/about">About us</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/contact">Contact</NavLink>
+              </NavItem>
+            </Nav>
+            </Collapse>
+          </Navbar>  
           <Switch>
             <Route exact path='/' component={ Homepage } />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
             <Route exact path='/administration' render={() => <Administration firebase={firebase}/> } />
           </Switch>
         </Container>
