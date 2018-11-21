40df970b6f21651180cf97bbadcea03ffaebcae1
Milan Cifra <milan3362@gmail.com>
2018-11-21 20:33:31 +0100

    page-heading

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default class About extends React.Component {
   render() {
     return (
       <div className="about">
-        <h2>About us</h2>
+        <h1 className="page-heading">About us</h1>
       </div>
     )
   }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ export default class Contact extends React.Component {
   render() {
     return (
       <div className="contact">
-        <h2>Contact</h2>
+        <h1 className="page-heading">Contact</h1>
       </div>
     )
   }
