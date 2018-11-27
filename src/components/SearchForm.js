e0b950ba110a328cfdc9d7b42f3460ef3b636cc1
Milan Cifra <milan3362@gmail.com>
2018-11-27 12:07:38 +0100

    search form - brands and models handling

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -7,10 +7,14 @@ export default class SearchForm extends React.Component {
     super(props);
     this.state = {
       redirect: false,
-      carClasses: { economic: true, middle: true, luxury: false, suv: false }
+      carClasses: { economic: true, middle: true, luxury: false, suv: false },
+      car_brand: "all",
+      car_model: "all"
     };
     this.submitForm = this.submitForm.bind(this);
     this.handleClassChange = this.handleClassChange.bind(this);
+    this.handleBrandChange = this.handleBrandChange.bind(this);
+    this.handleModelChange = this.handleModelChange.bind(this);
   }
   submitForm(e) {
     e.preventDefault();
@@ -22,6 +26,14 @@ export default class SearchForm extends React.Component {
     let prev = { ...this.state.carClasses };
     prev[target.name] = checked;
     this.setState({ carClasses: prev });
+  }
+  handleBrandChange(e) {
+    console.log(e.target.value);
+    this.setState({ car_brand: e.target.value, car_model: "all" });
+  }
+  handleModelChange(e) {
+    console.log(e.target.value);
+    this.setState({ car_model: e.target.value });
   }
   render() {
     return (
@@ -94,25 +106,36 @@ export default class SearchForm extends React.Component {
               <legend>Preferred car brand</legend>
               <div className="row form-group">
                 <div className="col-md-4">
-                  <label>Car brand</label>
+                  <label htmlFor="car-brand">Car brand</label>
                 </div>
                 <div className="col-md-8">
-                  <select className="form-control" name="select" id="car-brand">
-                    <option>All</option>
-                    <option>Skoda</option>
-                    <option>Tesla</option>
+                  <select onChange={this.handleBrandChange}
+                    defaultValue={this.state.car_brand}
+                    value={this.state.car_brand}
+                    className="form-control"
+                    name="car-brand"
+                    id="car-brand">
+                    <option value="all">All brands</option>
+                    <option value="skoda">Skoda</option>
+                    <option value="tesla">Tesla</option>
                   </select>
                 </div>
               </div>
               <div className="row form-group">
                 <div className="col-md-4">
-                  <label>Car model</label>
+                  <label htmlFor="car-model">Car model</label>
                 </div>
                 <div className="col-md-8">
-                  <Input type="select" name="select" id="car-brand">
-                    <option>Fabia</option>
-                    <option>Octavia</option>
-                  </Input>
+                  <select onChange={this.handleModelChange}
+                    defaultValue={this.state.car_model}
+                    value={this.state.car_brand === "all" ? "all" : this.state.car_model}
+                    className="form-control"
+                    name="car-model"
+                    id="car-model" disabled={this.state.car_brand === "all" ? true : false}>
+                    <option value="all">All from selected brand</option>
+                    <option value="fabia">Fabia</option>
+                    <option value="octavia">Octavia</option>
+                  </select>
                 </div>
               </div>
             </fieldset>
