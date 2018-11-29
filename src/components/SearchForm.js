93097f83b7211e062a0c81a7351b32b9675ffa6e
Milan Cifra <milan3362@gmail.com>
2018-11-29 21:47:24 +0100

    search form - car classes fetching and handling

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -45,12 +45,18 @@ export default class SearchForm extends React.Component {
     e.preventDefault();
     this.setState({ redirect: true });
   }
+
   handleClassChange(e) {
-    const target = e.target;
-    const checked = target.checked;
-    let prev = { ...this.state.carClasses };
-    prev[target.name] = checked;
-    this.setState({ carClasses: prev });
+    const class_id = e.target.name;
+    const checked = e.target.checked;
+    let prev = [...this.state.classes];
+    let x = prev.find(e => e.id === class_id);
+    if (x) {
+      x.selected = checked;
+    }
+    this.setState({
+      classes: prev
+    })
   }
 
   handleBrandChange(e) {
@@ -85,6 +91,7 @@ export default class SearchForm extends React.Component {
   }
 
   render() {
+    console.log(this.state);
     return (
       <React.Fragment>
         {this.state.redirect && (
@@ -96,133 +103,109 @@ export default class SearchForm extends React.Component {
         <div className="search-form">
           {this.state.render ? (
             <React.Fragment>
-          <h3>{this.props.title}</h3>
-          <form onSubmit={this.submitForm}>
-            <fieldset className="car-classes">
-              <legend>Car classes</legend>
-              <label className={this.state.carClasses['economic'] ? 'class-check class-checked' : 'class-check'}
-                htmlFor="class-economic">
-                <img
-                  className={this.state.carClasses['economic'] ? 'check-icon check-icon-checked' : 'check-icon'}
-                  alt="checked"
-                  src="./img/ok.png" />
-                <input
-                  type="checkbox" id="class-economic"
-                  name="economic"
-                  onChange={this.handleClassChange}
-                  checked={this.state.carClasses['economic']} />
-                Economic
-              </label>
-              <label className={this.state.carClasses['middle'] ? 'class-check class-checked' : 'class-check'}
-                htmlFor="class-middle">
-                <img
-                  className={this.state.carClasses['middle'] ? 'check-icon check-icon-checked' : 'check-icon'}
-                  alt="checked"
-                  src="./img/ok.png" />
-                <input
-                  type="checkbox" id="class-middle"
-                  name="middle"
-                  onChange={this.handleClassChange}
-                  checked={this.state.carClasses['middle']} />
-                Middle
-              </label>
-              <label className={this.state.carClasses['luxury'] ? 'class-check class-checked' : 'class-check'}
-                htmlFor="class-luxury">
-                <img
-                  className={this.state.carClasses['luxury'] ? 'check-icon check-icon-checked' : 'check-icon'}
-                  alt="checked"
-                  src="./img/ok.png" />
-                <input
-                  type="checkbox" id="class-luxury"
-                  name="luxury"
-                  onChange={this.handleClassChange}
-                  checked={this.state.carClasses['luxury']} />
-                Luxury
-              </label>
-            </fieldset>
-            <fieldset className="preferred-brand">
-              <legend>Preferred car brand</legend>
-              <div className="row form-group">
-                <div className="col-md-4">
-                  <label htmlFor="car-brand">Car brand</label>
-                </div>
-                <div className="col-md-8">
-                  <select onChange={this.handleBrandChange}
-                    value={this.state.brands.selected}
-                    className="form-control"
-                    name="car-brand"
-                    id="car-brand">
-                    <option value="all">All brands</option>
-                    <option value="skoda">Skoda</option>
-                    <option value="bmw">BMW</option>
-                  </select>
-                </div>
-              </div>
-              <div className="row form-group">
-                <div className="col-md-4">
-                  <label htmlFor="car-model">Car model</label>
-                </div>
-                <div className="col-md-8">
-                  <select onChange={this.handleModelChange}
-                    value={this.state.brands.selected === "all" ? "all" : this.state.models.selected}
-                    className="form-control"
-                    name="car-model"
-                    id="car-model"
-                    disabled={this.state.brands.selected === "all" ? true : false}>
-                    <option value="all">All from selected brand</option>
-                    {this.state.models.list.map(model =>
-                      <option key={model.id} value={model.id}>{model.name}</option>
+              <h3>{this.props.title}</h3>
+              <form onSubmit={this.submitForm}>
+                <fieldset className="car-classes">
+                  <legend>Car classes</legend>
+                  {this.state.classes.map(carClass =>
+                    <label className={carClass.selected ? 'class-check class-checked' : 'class-check'}
+                      htmlFor={"class-" + carClass.id}>
+                      <img
+                        className={carClass.selected ? 'check-icon check-icon-checked' : 'check-icon'}
+                        alt="checked"
+                        src="./img/ok.png" />
+                      <input
+                        type="checkbox" id={"class-" + carClass.id}
+                        name={carClass.id}
+                        onChange={this.handleClassChange}
+                        checked={carClass.checked} />
+                      {carClass.name}
+                    </label>
+                  )}
+                </fieldset>
+                <fieldset className="preferred-brand">
+                  <legend>Preferred car brand</legend>
+                  <div className="row form-group">
+                    <div className="col-md-4">
+                      <label htmlFor="car-brand">Car brand</label>
+                    </div>
+                    <div className="col-md-8">
+                      <select onChange={this.handleBrandChange}
+                        value={this.state.brands.selected}
+                        className="form-control"
+                        name="car-brand"
+                        id="car-brand">
+                        <option value="all">All brands</option>
+                        <option value="skoda">Skoda</option>
+                        <option value="bmw">BMW</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="row form-group">
+                    <div className="col-md-4">
+                      <label htmlFor="car-model">Car model</label>
+                    </div>
+                    <div className="col-md-8">
+                      <select onChange={this.handleModelChange}
+                        value={this.state.brands.selected === "all" ? "all" : this.state.models.selected}
+                        className="form-control"
+                        name="car-model"
+                        id="car-model"
+                        disabled={this.state.brands.selected === "all" ? true : false}>
+                        <option value="all">All from selected brand</option>
+                        {this.state.models.list.map(model =>
+                          <option key={model.id} value={model.id}>{model.name}</option>
+                        )}
+                      </select>
+                    </div>
+                  </div>
+                </fieldset>
+                <fieldset className="pick-up">
+                  <legend>Pick-up information</legend>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label htmlFor="pick-up-date">Pick-up date</label>
+                      <input className="form-control" id="pick-up-date" />
+                    </div>
+                    <div className="col-md-6">
+                      <label htmlFor="pick-up-time">Pick-up time</label>
+                      <input className="form-control" id="pick-up-time" />
+                    </div>
+                  </div>
+                  <label htmlFor="pick-up-location">Pick-up location</label>
+                  <select className="form-control"
+                    id="pick-up-location"
+                    onChange={this.handlePickUpPlace}
+                    value={this.state.places.pick_up}>
+                    {this.state.places.list.map(place =>
+                      <option value={place.id} key={place.id}>{place.name}</option>
                     )}
                   </select>
-                </div>
-              </div>
-            </fieldset>
-            <fieldset className="pick-up">
-              <legend>Pick-up information</legend>
-              <div className="row">
-                <div className="col-md-6">
-                  <label htmlFor="pick-up-date">Pick-up date</label>
-                  <input className="form-control" id="pick-up-date" />
-                </div>
-                <div className="col-md-6">
-                  <label htmlFor="pick-up-time">Pick-up time</label>
-                  <input className="form-control" id="pick-up-time" />
-                </div>
-              </div>
-              <label htmlFor="pick-up-location">Pick-up location</label>
-              <select className="form-control"
-                id="pick-up-location"
-                onChange={this.handlePickUpPlace}
-                value={this.state.places.pick_up}>
-                {this.state.places.list.map(place =>
-                  <option value={place.id} key={place.id}>{place.name}</option>
-                )}
-              </select>
-            </fieldset>
-            <fieldset className="drop-off">
-              <legend>Drop-off information</legend>
-              <div className="row">
-                <div className="col-md-6">
-                  <label htmlFor="drop-off-date">Drop-off date</label>
-                  <input className="form-control" id="drop-off-date" />
-                </div>
-                <div className="col-md-6">
-                  <label htmlFor="drop-off-time">Drop-off time</label>
-                  <input className="form-control" id="drop-off-time" />
-                </div>
-              </div>
-              <label htmlFor="drop-off-location">Drop-off location</label>
-              <select className="form-control"
-                id="drop-off-location"
-                onChange={this.handleDropOffPlace}
-                value={this.state.places.drop_off}>
-                {this.state.places.list.map(place =>
-                  <option value={place.id} key={place.id}>{place.name}</option>
-                )}
-              </select>
-            </fieldset>
-            <button className="btn btn-primary">Search</button>
-          </form>
+                </fieldset>
+                <fieldset className="drop-off">
+                  <legend>Drop-off information</legend>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label htmlFor="drop-off-date">Drop-off date</label>
+                      <input className="form-control" id="drop-off-date" />
+                    </div>
+                    <div className="col-md-6">
+                      <label htmlFor="drop-off-time">Drop-off time</label>
+                      <input className="form-control" id="drop-off-time" />
+                    </div>
+                  </div>
+                  <label htmlFor="drop-off-location">Drop-off location</label>
+                  <select className="form-control"
+                    id="drop-off-location"
+                    onChange={this.handleDropOffPlace}
+                    value={this.state.places.drop_off}>
+                    {this.state.places.list.map(place =>
+                      <option value={place.id} key={place.id}>{place.name}</option>
+                    )}
+                  </select>
+                </fieldset>
+                <button className="btn btn-primary">Search</button>
+              </form>
             </React.Fragment>
           ) : (
               <p>Wait for it ...</p>
