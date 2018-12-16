360ade6970b34ad8e8a959dcf03410e1ab3cfe77
Milan Cifra <milan3362@gmail.com>
2018-12-16 21:08:27 +0100

    delete comment

--- src/components/Results.js
+++ src/components/Results.js
@@ -60,9 +60,7 @@ export default class Results extends React.Component {
         <div className="row">
           {this.state.cars.map(car =>
             <div className="col-sm-6 col-lg-4 px-2 grid-item" key={car.id}>
-              {/* <Link to={`/car/${car.id}`}> */}
-                <CarThumb carData={car} />
-              {/* </Link> */}
+              <CarThumb carData={car} />
             </div>
           )}
         </div>
